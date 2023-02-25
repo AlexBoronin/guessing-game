9508a9fa35398d790e9e52a6f2951e676644190f
AlexBoronin <acivinel@mail.ru>
2023-02-25 23:37:45 +0300

    end

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.ceil((this.max + this.min)/ 2)
     }
 
     lower() {
-
+        return this.max = Math.ceil((this.max + this.min) /2)
     }
 
     greater() {
-
+        return this.min = Math.ceil((this.max + this.min) /2)
     }
 }
 
 module.exports = GuessingGame;
+// Бинарный поиск, один метод принимает границы, второй выдаёт число, и два крайних смещают минимальную
+// и максимальную границу пока не получите нужное число
